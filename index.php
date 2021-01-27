f5eb3d73a4f0f4047e322d650f067662a660b5ff
artikhovich <artikhovich.by@gmail.com>
2021-01-27 16:28:07 +0300

    few changes 270121

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php print 'php out very well'; ?>
+<?php 
+class Entree {
+	public $name;
+	public $ingredients = array();
+	public function __construct($name, $ingredients){
+		if (! is_array($ingredients)){
+			throw new Exception('$ingredients must be an array');
+		}
+		$this->name = $name;
+		$this->ingredients = $ingredients;
+	}
+	
+	public function hasIngredient($ingredients) {
+		return in_array($ingredient, $this->ingredients);
+	}
+}
+
+$soup = new Entree;
+$soup->name = 'Chiken Soup';
+$soup->ingredients = array('chicken','water','fruits');
+
+
+print 'php out very well';
+
+freach ()
+?>
