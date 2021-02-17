d883fbbdbd23241b8c90bbe6a90d3fd11a25dcd0
artikhovich <artikhovich.by@gmail.com>
2021-02-17 19:10:27 +0300

    few changes for getjson

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-function productsDataSort(data){
+function productsDataBegin(){
 	let read_products_html = ``;
 		read_products_html+=`<table class="products"><tbody>`;
 		read_products_html+=`<tr>
@@ -8,6 +8,33 @@ function productsDataSort(data){
 				<th class='head'>Категория</th>
 				<th class='head'>Цена</th>
 		</tr>`;
+	return read_products_html;
+}
+
+function productsDataLines(data){
+	$.each(data.records, function(key,val){
+		let read_products_html = ``;
+		read_products_html+=`<tr>
+			<td>`+val.id+`</td>
+			<td>`+val.name+`</td>
+			<td>`+val.description+`</td>
+			<td>`+val.category_name+`</td>
+			<td>`+val.price+`</td>
+		</tr>`;
+	});
+}
+
+function productsDataSort(data){
+		let read_products_html = ``;
+		read_products_html+=`<table class="products"><tbody>`;
+		read_products_html+=`<tr>
+				<th class='head'>ID</th>
+				<th class='head'>Название</th>
+				<th class='head'>Описание</th>
+				<th class='head'>Категория</th>
+				<th class='head'>Цена</th>
+		</tr>`;
+
 		$.each(data.records, function(key,val){
 			read_products_html+=`<tr>
 				<td>`+val.id+`</td>
@@ -17,33 +44,40 @@ function productsDataSort(data){
 				<td>`+val.price+`</td>
 			</tr>`;
 		});
-	read_products_html+=`</tbody></table>`;
-	// console.log("productDataSort: "+read_products_html);
-	return read_products_html;
+		read_products_html+=`</tbody></table>`;
+		return read_products_html;
+		// return data;
 }
 
 function showProducts(){
 	$.getJSON("includes/product/read_product.php", function(data){		
-		$("#products-content").html(productsDataSort(data));
+		// $("#products-content").html(productsDataSort(data));
+		console.log(data);
+		return data;
 	});
-	// return read_products_html;
 };
 
 
 function vanillaProducts(){
-	console.log ("button clicked");
+	// console.log("button clicked");
 	var request = new XMLHttpRequest();
-	request.open('Get', 'includes/product/read_product.php');
-	request.onreadystatechange = function() {
+	// request.responseType =	"json";
+	request.open('GET', 'includes/product/read_product.php', false);
+	request.send();
+		if(request.status === 200) {
+				let phpOut = JSON.parse(request.responseText);
+				// console.log(phpOut);
+				let contentOut = productsDataSort(phpOut);
+				// console.log('contentOut from vanillaProducts: ' + contentOut);
+				// document.getElementById('vanilla-content').innerHtml = contentOut;
+				// console.log(phpOut);
+				console.log(contentOut);
 
-			if(request.status === 200) {
-				let contentOut = productDataSort(request.responseText);
-				console.log('contentOut from vanillaProducts: ' + contentOut);
-				document.getElementById('vanilla-content').innerHtml = contentOut;
+				return "phpOut";
+				// return contentOut;
 			} else {
 			console.log('Ann error in vanillaProducts');
 			}
-	}
 }
 
 var contentApp = `
@@ -55,17 +89,15 @@ var contentApp = `
 function addClickEvent(data){
 	console.log('mousedown');
 	document.getElementById('vanilla-content').innerHTML=data;
-	// .innerHTML = 'Новый BODY!'
 }
+
+var clickerText='<h1>Boss</h1>';
 
 document.addEventListener("DOMContentLoaded",function(){
 	$("#app").html(contentApp);
-	// $("#products-content").html(showProducts());
-	// console.log("All DOM Loaded");
-
 	requestBtn.onclick=function(){
-	showProducts();
-		addClickEvent("Hello Clicker!!!");
+		showProducts();
+		addClickEvent(clickerText);
 	}
 
 });
